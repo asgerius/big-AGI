d1224a5d2de275b55b0fb0b26f67138845d817d0
Asger Schultz <asger.s@protonmail.com>
2024-03-28 21:03:45 +0100

    Bedre navngivning

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,7 +8,7 @@
  */
 export const Brand = {
   Title: {
-    Base: 'big-AGI',
+    Base: 'Asgers fantastiske KGI',
     Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
   },
   Meta: {
